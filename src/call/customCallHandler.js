4b9075130b34de67a82dbbc09f0ae6175ab2dd13
ZombiMigz <chunyumikey@gmail.com>
2021-05-13 14:46:55 -0700

    completes custom named call feature

--- src/call/customCallHandler.js
+++ src/call/customCallHandler.js
@@ -2,23 +2,62 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initCustomCallHandler = void 0;
 var bot_1 = require("../../bot");
-var createChannelID = require('../channelIDs.json')["Custom Call Creator"];
-var createChannel;
+var callHandler_1 = require("./callHandler");
+var channelList = require('../channelIDs.json');
+var createChannelID = channelList["Custom Call Creator"];
+var channel;
 var tutorialMsg = "To create your own channel, type ```.create [channelname]``` Make sure to join the channel in 15 seconds or it will delete itself. Have fun!";
+var msgLengthError = "Your channel name was too long. Please try a shorter name. :)";
+var misunderstoodError = "Sorry, I didn't understand.";
 function initCustomCallHandler() {
-    console.log("create channel id " + createChannelID);
     bot_1.client.channels.fetch(createChannelID)
         .then(function (res) {
-        createChannel = res;
-        createChannel.bulkDelete(100).catch(console.log);
-        createChannel.send(tutorialMsg);
+        channel = res;
+        channel.bulkDelete(100).catch(console.log);
+        channel.send(tutorialMsg);
     })
         .catch(function (err) { return console.log("Failed to find call creation text channel"); });
     bot_1.client.on('message', function (msg) { readMessage(msg); });
 }
 exports.initCustomCallHandler = initCustomCallHandler;
 function readMessage(msg) {
-    /*if (createChannelID == msg.channel.id) {
-        
-    }*/
+    if (msg.author.bot)
+        return;
+    if (createChannelID == msg.channel.id) {
+        if (msg.content.split(' ')[0] == ".create") {
+            if (msg.content.length > 100)
+                sendError(msg, msgLengthError);
+            var name_1 = msg.content.substr(8);
+            createChannel(name_1);
+            msg.delete();
+        }
+        sendError(msg, misunderstoodError + ' ' + tutorialMsg);
+    }
+}
+function createChannel(name) {
+    bot_1.client.channels.fetch(channelList["Voice Call Category"])
+        .then(function (res) {
+        var category = res;
+        var guild = category.guild;
+        guild.channels.create(name, { type: "voice" }).then(function (channel) {
+            channel.setParent(category);
+            setTimeout(function () { checkChannel(channel); }, 10000);
+        });
+    })
+        .catch(console.log);
+}
+function checkChannel(channel) {
+    if (channel.members.size < 1) {
+        channel.delete();
+        return;
+    }
+    callHandler_1.addTempChannel(channel.id);
+}
+function sendError(msg, error) {
+    channel.send(error)
+        .then(function (res) {
+        res.delete({ timeout: 5000 });
+    })
+        .catch(console.log);
+    msg.delete({ timeout: 5000 });
 }
--- src/call/customCallHandler.ts
+++ src/call/customCallHandler.ts
@@ -1,26 +1,66 @@
-import { Channel, Client, DiscordAPIError, Message, TextChannel } from "discord.js";
+import { APIMessageContentResolvable, CategoryChannel, Channel, Client, DiscordAPIError, Guild, Message, TextChannel, VoiceChannel } from "discord.js";
 import { client } from "../../bot";
+import { addTempChannel } from "./callHandler";
 
-
-const createChannelID: string = require('../channelIDs.json')["Custom Call Creator"]
-let createChannel: TextChannel;
+const channelList = require('../channelIDs.json');
+const createChannelID: string = channelList["Custom Call Creator"]
+let channel: TextChannel;
 
 const tutorialMsg = "To create your own channel, type ```.create [channelname]``` Make sure to join the channel in 15 seconds or it will delete itself. Have fun!";
+const msgLengthError = "Your channel name was too long. Please try a shorter name. :)"
+const misunderstoodError = "Sorry, I didn't understand."
 
 export function initCustomCallHandler() {
-    console.log("create channel id " + createChannelID);
     client.channels.fetch(createChannelID)
         .then((res) => {
-            createChannel = <TextChannel> res;
-            createChannel.bulkDelete(100).catch(console.log);
-            createChannel.send(tutorialMsg);
+            channel = <TextChannel> res;
+            channel.bulkDelete(100).catch(console.log);
+            channel.send(tutorialMsg);
         })
         .catch((err) => console.log("Failed to find call creation text channel"));
     client.on('message', (msg) => {readMessage(msg)})
 }
 
 function readMessage(msg: Message) {
-    /*if (createChannelID == msg.channel.id) {
-        
-    }*/
+    if (msg.author.bot) return;
+    if (createChannelID == msg.channel.id){
+        if (msg.content.split(' ')[0] == ".create") {
+            if (msg.content.length > 100) sendError(msg, msgLengthError);
+            let name:string = msg.content.substr(8);
+            createChannel(name);
+            msg.delete();
+        }
+        sendError(msg, misunderstoodError + ' ' + tutorialMsg);
+    } 
+}
+
+function createChannel(name: string) {
+    client.channels.fetch(channelList["Voice Call Category"])
+    .then((res) => {
+        let category: CategoryChannel = <CategoryChannel> res;
+        let guild:Guild = category.guild;
+        guild.channels.create(name, {type: "voice"}).then(channel => {
+            channel.setParent(category);
+            setTimeout(() => {checkChannel(channel)}, 10000);
+        })
+    })
+    .catch(console.log);
+    
+}
+
+function checkChannel(channel: VoiceChannel) {
+    if (channel.members.size < 1) {
+        channel.delete();
+        return;
+    }
+    addTempChannel(channel.id);
+}
+
+function sendError(msg: Message, error: String) {
+    channel.send(<APIMessageContentResolvable> error)
+        .then((res: Message) => {
+            res.delete({timeout: 5000});
+        })
+        .catch(console.log);
+    msg.delete({timeout:5000});
 }
