d1bbdbc2fc35c2267463a5cf137f1f71fe713b14
ZombiMigz <chunyumikey@gmail.com>
2021-05-15 09:54:50 -0700

    removes sequelize

--- database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-let database;
-
-module.exports = {
-    init: () => database = new Sequelize(os.environ.get('DATABASE_URL')),
-    getDatabase: () => {return database}
-}
-
