1a91ad60a7c6c02f9baea0037afd15ba150d20b3
ZombiMigz <chunyumikey@gmail.com>
2021-06-05 13:04:01 -0700

    adds auto birthday file creation and backup

--- src/birthday/birthdayStorage.js
+++ src/birthday/birthdayStorage.js
@@ -1,16 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.exportBDays = exports.importBDays = void 0;
+exports.getBDay = exports.initBDayStorage = void 0;
 var fs_1 = require("fs");
 var bDays;
 var filePath = "src/birthday/birthdays.json";
-function importBDays() {
-    bDays = JSON.parse(fs_1.readFileSync(filePath).toString());
+var backupFilePath = "src/birthday/backup.json";
+function initBDayStorage() {
+    importBDays();
 }
-exports.importBDays = importBDays;
+exports.initBDayStorage = initBDayStorage;
+function importBDays() {
+    var str = fs_1.readFileSync(filePath, { flag: 'a+' }).toString();
+    ;
+    try {
+        bDays = JSON.parse(str);
+    }
+    catch (err) {
+        console.warn("Unable to read bDay file: " + bDays);
+        console.warn("making new bDay file");
+        fs_1.appendFile(backupFilePath, str, function (res) { return "Backed up data to " + backupFilePath + ": \n" + res; });
+    }
+}
 function exportBDays() {
     fs_1.writeFile(filePath, JSON.stringify(bDays), function (err) {
         console.log("Error exporting birthday file: " + err);
     });
 }
-exports.exportBDays = exportBDays;
+function getBDay(id) {
+    return bDays.find(function (value) {
+        return value.id == id ? true : false;
+    });
+}
+exports.getBDay = getBDay;
--- src/birthday/birthdayStorage.ts
+++ src/birthday/birthdayStorage.ts
@@ -1,15 +1,39 @@
 import { User } from "discord.js";
-import { readFileSync, writeFile } from "fs";
+import { appendFile, readFileSync, writeFile } from "fs";
 
-let bDays: Object;
-let filePath = "src/birthday/birthdays.json"
-
-export function importBDays() {
-    bDays = JSON.parse(readFileSync(filePath).toString());
+interface bDay {
+    id: string,
+    day: number;
 }
 
-export function exportBDays() {
+let bDays: bDay[];
+let filePath = "src/birthday/birthdays.json";
+let backupFilePath = "src/birthday/backup.json";
+
+export function initBDayStorage() {
+    importBDays();
+}
+
+function importBDays() {
+    let str: string = readFileSync(filePath, {flag: 'a+'}).toString();;
+    try {
+        bDays = JSON.parse(str);
+    }
+    catch(err) {
+        console.warn(`Unable to read bDay file: ${bDays}`);
+        console.warn(`making new bDay file`);
+        appendFile(backupFilePath, str, res => `Backed up data to ${backupFilePath}: \n${res}`);
+    }
+}
+
+function exportBDays() {
     writeFile(filePath, JSON.stringify(bDays), err => {
         console.log(`Error exporting birthday file: ${err}`);
+    })
+}
+
+export function getBDay(id: string) {
+    return bDays.find(value => {
+        return value.id == id? true: false;
     })
 }
