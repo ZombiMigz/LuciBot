4a4f703cc76442521083a7f9956e8d4bc0339f9c
ZombiMigz <chunyumikey@gmail.com>
2021-06-05 20:05:30 -0700

    adds send time to ping command

--- src/debug/debug.js
+++ src/debug/debug.js
@@ -15,7 +15,10 @@ exports.initDebugger = initDebugger;
 function pingListener() {
     bot_1.client.on('message', function (msg) {
         if (msg.channel.id == debugChannel.id && msg.content.startsWith("ping")) {
-            debugChannel.send("Time between send and receive: " + (new Date().getTime() - msg.createdAt.getTime()) + " ms");
+            var timeSent_1 = new Date().getTime();
+            debugChannel.send("Time to register msg: " + (timeSent_1 - msg.createdAt.getTime()) + " ms").then(function (msg) {
+                debugChannel.send("Time to send msg: " + (msg.createdTimestamp - timeSent_1));
+            });
         }
     });
 }
--- src/debug/debug.ts
+++ src/debug/debug.ts
@@ -15,7 +15,10 @@ export function initDebugger() {
 function pingListener() {
     client.on('message', (msg: Message) => {
         if (msg.channel.id == debugChannel.id && msg.content.startsWith("ping")) {
-            debugChannel.send(`Time between send and receive: ${new Date().getTime() - msg.createdAt.getTime()} ms`);
+            let timeSent:number = new Date().getTime();
+            debugChannel.send(`Time to register msg: ${timeSent - msg.createdAt.getTime()} ms`).then(msg => {
+                debugChannel.send(`Time to send msg: ${msg.createdTimestamp - timeSent}`)
+            });
         }
     })
 }
