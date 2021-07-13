42990b4a7ae7cd3dbb40ceb35268c15cae26c5e8
Luciano Lim <chunyumikey@gmail.com>
2021-07-13 16:33:31 -0700

    web endpoint setup

--- src/web/webEndpoint.js
+++ src/web/webEndpoint.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initWebEndpoint = void 0;
+var cors_1 = __importDefault(require("cors"));
 var express_1 = __importDefault(require("express"));
-var port = 3000;
 var endpoint = express_1.default();
+endpoint.use(express_1.default.json());
+endpoint.use(cors_1.default());
 function initWebEndpoint() {
     test();
     init();
@@ -14,9 +16,11 @@ function initWebEndpoint() {
 exports.initWebEndpoint = initWebEndpoint;
 function test() {
     endpoint.post("/test", function (req, res) {
+        console.log("post received");
+        res.send("received");
         console.log(req.body);
     });
 }
 function init() {
-    endpoint.listen(port, function () { return console.log("web endpoint initiated"); });
+    endpoint.listen(3000, function () { return console.log("web endpoint initiated"); });
 }
--- src/web/webEndpoint.ts
+++ src/web/webEndpoint.ts
@@ -1,20 +1,23 @@
+import cors from 'cors';
 import express from 'express';
 
-const port = 3000;
 const endpoint = express();
+endpoint.use(express.json());
+endpoint.use(cors());
 
 export function initWebEndpoint() {
   test();
 
   init();
 }
-
 function test() {
   endpoint.post("/test", (req, res) => {
+    console.log("post received");
+    res.send("received");
     console.log(req.body);
   });
 }
 
 function init() {
-  endpoint.listen(port, () => console.log("web endpoint initiated"));
+  endpoint.listen(3000, () => console.log("web endpoint initiated"));
 }
