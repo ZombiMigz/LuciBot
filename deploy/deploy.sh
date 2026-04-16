c0156dc74b501c134e5b14e1a8d6866086836eb2
Luciano Lim <contact@lucianolim.me>
2026-04-15 20:20:23 -0700

    Forces repull of docker

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,9 +8,15 @@ IMAGE="us-west1-docker.pkg.dev/lucibot-493305/lucibot/lucibot:latest"
 
 echo "Deploying $IMAGE to $INSTANCE ($ZONE)..."
 
-gcloud compute instances update-container "$INSTANCE" \
+# Pull latest image and restart container on the VM
+gcloud compute ssh "$INSTANCE" \
   --project="$PROJECT" \
   --zone="$ZONE" \
-  --container-image="$IMAGE"
+  --command="
+    docker-credential-gcr configure-docker --registries=us-west1-docker.pkg.dev &&
+    docker pull $IMAGE &&
+    docker ps -q | xargs -r docker stop &&
+    docker run -d --restart=always $IMAGE
+  "
 
 echo "Deploy complete."
